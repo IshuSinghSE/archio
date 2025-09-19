ff3ab714458b37ee74cfeb52656116a7a0e08f1c
IshuSinghSE <ishu.111636@gmail.com>
2025-09-19 23:03:36 +0530

    fix bug

--- config.example.js
+++ config.example.js
@@ -2,5 +2,6 @@
 const CONFIG = {
     AIO_USERNAME: "YOUR_ADAFRUIT_USERNAME",
     AIO_KEY: "YOUR_ADAFRUIT_IO_KEY",
-    FEED_NAME: "jarvis-commands"
+    FEED_NAME: "jarvis-commands",
+    AIO_FEEDBACK_ID: "agent-feedback" // The feed your agent sends responses FROM
 };
